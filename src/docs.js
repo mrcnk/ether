6af6a05337aa86eaba9cfb2804b67c90e33f0d0e
Thomas Marciniak <tomasz.marciniak@setapp.pl>
2018-05-08 16:37:01 +0200

    Add radio and checkbox

--- src/docs.js
+++
@@ -1,5 +0,0 @@
-const parser = require('comment-parser')
-const fs = require('file-system')
-
-const buffer = fs.readFileSync(__dirname + '/stylus/elements/typography/headers.styl', 'utf-8')
-console.log(parser.parse(buffer))
