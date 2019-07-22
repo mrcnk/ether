52d81a63f8317ab04990bdf529b584b673c70c96
Thomas Marciniak <tmarciniakm@gmail.com>
2019-07-22 22:30:17 +0200

    Update docs

--- docs/ether.db4c18ae.js
+++ docs/ether.db4c18ae.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"U1PN":[function(require,module,exports) {
 "use strict";require("./ether.styl");
 },{"./ether.styl":"ziWM"}]},{},["U1PN"], null)
-//# sourceMappingURL=/ether.db4c18ae.js.map
+//# sourceMappingURL=ether.db4c18ae.js.map
